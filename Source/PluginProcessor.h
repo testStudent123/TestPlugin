9048bb33639ae6a4f358c15e9f2efd94fc32ea45
David Moore <j.d.moore@gcu.ac.uk>
2019-02-24 09:53:21 +0000

    Update PluginProcessor.h
    Added a gain variable

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -55,6 +55,7 @@ public:
     void getStateInformation (MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
+    float gain;
 private:
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TestPluginAudioProcessor)
